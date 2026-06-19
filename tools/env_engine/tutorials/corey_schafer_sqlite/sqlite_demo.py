00c849884d9d24f100caf2130d97c1888ea272cb
basileog-codes <basileogwork@gmail.com>
2026-06-19 17:48:01 +0700

    Study: Add initial raw SQL record insertion

--- tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
+++ tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
@@ -4,11 +4,13 @@ conn = sqlite3.connect ('employee.db')
 
 c = conn.cursor ()
 
-c.execute ("""CREATE TABLE employees (
-           first text,
-           last text,
-           pay integer
-           )""" )
+# c.execute ("""CREATE TABLE employees (
+#            first text,
+#            last text,
+#            pay integer
+#            )""" )
+
+c.execute ("INSERT INTO employees VALUES ('Basile', 'Grégoire', 4000)")
 
 conn.commit ()
 
