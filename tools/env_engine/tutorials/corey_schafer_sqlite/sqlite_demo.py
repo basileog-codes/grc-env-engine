a18f08cb37847d7342a73b08d80f36fd75d17049
basileog-codes <basileogwork@gmail.com>
2026-06-21 19:21:08 +0700

    Study: Refactor database operations into CRUD functions using named placeholders and context managers

--- tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
+++ tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
@@ -11,8 +11,42 @@ c = conn.cursor ()
 #            pay integer
 #            )""" )
 
-emp_1 = Employee ('Paul', 'McCartney', 6500)
-emp_2 = Employee ('John', 'Lennon', 9000)
+def insert_emp (emp):
+    with conn:
+        c.execute ("INSERT INTO employees VALUES (:first, :last, :pay)",
+                   {'first': emp.first, 'last': emp.last, 'pay': emp.pay})
+
+
+def get_emps_by_name (lastname):
+    c.execute ("SELECT * FROM employees WHERE last = :last", {'last': lastname})
+    return c.fetchall ()
+
+def update_pay (emp, pay):
+    with conn:
+        c.execute ("""UPDATE employees SET pay = :pay
+                   WHERE first = :first AND last = :last""",
+                   {'first': emp.first, 'last': emp.last, 'pay': pay})
+
+def remove_emp (emp):
+    with conn:
+        c.execute ("""DELETE from employees WHERE first = :first AND last = :last""",
+                   {'first': emp.first, 'last': emp.last})
+
+
+emp_1 = Employee ('Jean-Louis', 'Grégoire', 6500)
+emp_2 = Employee ('Jean-Pierre', 'Grégoire', 9000)
+
+insert_emp (emp_1)
+insert_emp (emp_2)
+
+emps = get_emps_by_name ('Grégoire')
+print (emps)
+
+update_pay (emp_2, 11000)
+remove_emp (emp_1)
+
+emps = get_emps_by_name ('Grégoire')
+print (emps)
 
 # print (emp_1.first)
 # print (emp_1.last)
@@ -20,7 +54,7 @@ emp_2 = Employee ('John', 'Lennon', 9000)
 
 # c.execute ("INSERT INTO employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
 
-conn.commit ()
+# conn.commit ()
 
 # - DATA DUPLICATE DELETION - START
 # c.execute ("SELECT rowid, * FROM employees WHERE last = 'McCartney'")
@@ -29,19 +63,15 @@ conn.commit ()
 # c.execute ("DELETE FROM employees WHERE rowid = :id_to_remove", {'id_to_remove': 4})
 # - DATA DUPLICATION DELETION - END
 
-c.execute ("INSERT INTO employees VALUES (:first, :last, :pay)",
-           {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
+# c.execute ("INSERT INTO employees VALUES (:first, :last, :pay)",
+#            {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
 
-conn.commit ()
+# conn.commit ()
 
 # c.execute ("INSERT INTO employees VALUES ('Jean-Marie', 'Grégoire', 6000)")
 
-# conn.commit()
+# conn.commit() 
 
-c.execute ("SELECT * FROM employees WHERE last = 'Lennon'") 
-
-print (c.fetchall ())
-
-conn.commit ()
+# conn.commit ()
 
 conn.close ()
