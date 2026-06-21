e30ef112527c25d6c94e481e4bc8943c03c06fae
basileog-codes <basileogwork@gmail.com>
2026-06-21 17:39:34 +0700

    Study: Fix duplicate employee issue in database:- Targeted exact duplicate using SQLite hidden rowid- Added conn.commit() to ensure the deletion saves to the database file

--- tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
+++ tools/env_engine/tutorials/corey_schafer_sqlite/sqlite_demo.py
@@ -14,15 +14,26 @@ c = conn.cursor ()
 emp_1 = Employee ('Paul', 'McCartney', 6500)
 emp_2 = Employee ('John', 'Lennon', 9000)
 
-print (emp_1.first)
-print (emp_1.last)
-print (emp_1.pay)
+# print (emp_1.first)
+# print (emp_1.last)
+# print (emp_1.pay)
+
+# c.execute ("INSERT INTO employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
+
+# conn.commit ()
+
+# c.execute ("SELECT rowid, * FROM employees WHERE last = 'McCartney'")
+# print (c.fetchall())
+
+c.execute ("DELETE FROM employees WHERE rowid = :id_to_remove", {'id_to_remove': 4})
+
+# c.execute ("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': emp_2.first, 'last': emp_2.last, 'pay': emp_2.pay})
 
 # c.execute ("INSERT INTO employees VALUES ('Jean-Marie', 'Grégoire', 6000)")
 
 # conn.commit()
 
-# c.execute ("SELECT * FROM employees WHERE last = 'Grégoire'") 
+# c.execute ("SELECT * FROM employees WHERE last = 'McCartney'") 
 
 # print (c.fetchall ())
 
