a5a3d2663d4324496dbe98a6baef85e13296046d
basileog-codes <basileogwork@gmail.com>
2026-07-05 17:11:30 +0700

    Study: Add basic Theater class blueprint

---
+++ tools/env_engine/tutorials/simpy_practice.py
@@ -0,0 +1,24 @@
+# Movie Theater Simulation
+# Goal - Average Wait - 10 minutes or less
+
+import simpy
+import random
+import statistics
+
+wait_times = []
+
+class Theater (object):
+    def __init__ (self, env, num_cashiers, num_servers, num_ushers):
+        self.env = env
+        self.cashier = simpy.Resource (env, num_cashiers)
+        self.servers = simpy.Resource (env, num_servers)
+        self.ushers = simpy.Resource (env, num_ushers)
+    
+    def purchase_ticket (self, moviegoer):
+        yield self.env.timeout (random.randint (1, 3))
+
+    def check_ticket (self, moviegoer):
+        yield self.env.timeout (3 / 60)
+
+    def sell_food (self, moviegoer):
+        yield self.env.timeout (random.randint (1, 6))
