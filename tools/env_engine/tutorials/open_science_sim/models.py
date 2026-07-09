f8c35f5ce7723ca8ae02736a2e20020d33208129
basileog-codes <basileogwork@gmail.com>
2026-07-09 15:23:51 +0700

    Study: Complete prescription arrival exercise with unseeded and seeded models

--- tools/env_engine/tutorials/open_science_sim/models.py
+++ tools/env_engine/tutorials/open_science_sim/models.py
@@ -60,4 +60,59 @@ env.process (arrivals_generator (env))
 env.run (until = RUN_LENGTH)
 print (f'end of run. simulation clock time = {env.now}')
 
+# ----------------------------------------------------------------------------------------
+# Exercise: Modelling a poison arrival for prescriptions
+# Task:
+# Update arrivals_generator () so that the inter-arrival times follow an exponential
+# distribution with a mean inter-arrival time of 60.0 minutes / 100 arrivals between arrivals
+# (i.e. 100 arrivals per hour). Use a run length of 25 times.
 
+# Bonus challenge:
+# - First, try to implement this without setting a random seed
+# - Then, update the method with an approach to control the randomness.
+
+# Hints:
+# - We learnt how to sample things using a numpy random number generator in the sampling
+# notebook. Excluding a random seed, the basic method for drawing a single sample follows
+# this pattern:
+# rng = np.random.default_rng ()
+# sample = rng.exponential (scale = 12.0)
+
+# Exercise Solution
+
+# %% [CELL 1: Imports]
+import numpy as np
+
+# %% [CELL 2: RNG generator]
+# Without seed
+# rng = np.random.default_rng ()
+
+# With seed
+rng = np.random.default_rng (60)
+
+# %% [CELL 2: Arrival generator]
+def arrivals_generator (env):
+    while True:        
+        samples = rng.exponential (scale = 0.6)
+        inter_arrival_time = samples
+        yield env.timeout (inter_arrival_time)
+        print (f'poison arrives at: {env.now}')
+
+# %% [CELL 3: Simulation runs]
+RUN_LENGTH = 25
+env = simpy.Environment ()
+env.process (arrivals_generator (env))
+env.run (until = RUN_LENGTH)
+print (f'end of simulation run. clock time = {env.now}')
+
+
+
+
+
+
+
+
+
+
+
+# %%
